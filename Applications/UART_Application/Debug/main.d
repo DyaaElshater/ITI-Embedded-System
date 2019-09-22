3bac144b2f70b69b525b53fab98185c4fe102b70
DyaaElshater <37374922+DyaaElshater@users.noreply.github.com>
2019-09-22 10:09:19 +0200

    Add files via upload

---
+++ Applications/UART_Application/Debug/main.d
@@ -0,0 +1,10 @@
+main.o main.o: ../main.c ../Std_types.h ../Bit_Math.h ../DIO_int.h \
+  ../UART_int.h
+
+../Std_types.h:
+
+../Bit_Math.h:
+
+../DIO_int.h:
+
+../UART_int.h:
---
+++ Applications/User/Debug/main.d
@@ -0,0 +1,7 @@
+main.o main.o: ../main.c \
+  F:\Courses\Embedded\ ITI\Lab\Lec\Architecture\lib\Bit_Math.h \
+  F:\Courses\Embedded\ ITI\Lab\Lec\Architecture\lib\Std_types.h
+
+F:\Courses\Embedded\ ITI\Lab\Lec\Architecture\lib\Bit_Math.h:
+
+F:\Courses\Embedded\ ITI\Lab\Lec\Architecture\lib\Std_types.h:
