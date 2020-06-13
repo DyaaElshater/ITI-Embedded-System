d2b78554712947009b604cfb2d4626d338faa151
DyaaElshater <37374922+DyaaElshater@users.noreply.github.com>
2020-06-13 08:45:21 +0200

    delay

--- ARM/STM32f429zi/Architecture/5.lib/DELAY.h
+++ ARM/STM32f429zi/Architecture/5.lib/DELAY.h
@@ -1,64 +1,30 @@
 /*
  * DELAY.h
  *
- *  Created on: Jun 13, 2020
+ *  Created on: Oct 21, 2019
  *      Author: Dyaa Elshater
  */
 
-	/*The delay for 8 MHZ Frequency*/
 #ifndef DELAY_H_
 #define DELAY_H_
 
-typedef struct
-{
-	unsigned int CTRL;
-	unsigned int LOAD;
-	unsigned int VAL;
-	unsigned int CALIB;
-}STK_Reg;
+#define delay_us(time) do{u32 Loc_u32var,Loc_u32Max=time<<2;\
+							for(Loc_u32var=0;Loc_u32var<Loc_u32Max;Loc_u32var++)\
+							{__asm__("NOP");\
+							}\
+						}while(0)
 
-#define SYSTICK ((volatile STK_Reg *)(0XE000E010))
-static unsigned char __SYSTICK_Flag_ =0;
+#define delay_ms(time) do{u32 Loc_u32var,Loc_u32Max=(time<<2)*1000;\
+							for(Loc_u32var=0;Loc_u32var<Loc_u32Max;Loc_u32var++)\
+							{__asm__("NOP");\
+							}\
+						}while(0)
 
-void Delay_vidInit()
-{
-	/*Enable exception + clock source = processor clk*/
-	SYSTICK->CTRL = 6;
-}
+#define delay_s(time) do{u32 Loc_u32var,Loc_u32Max=(time<<2)*1000000;\
+							for(Loc_u32var=0;Loc_u32var<Loc_u32Max;Loc_u32var++)\
+							{__asm__("NOP");\
+							}\
+						}while(0)
 
-void Delay_us(unsigned int u32DelValueCpy)
-{
-	/*clk cycle = 125 ns -> 1 us = clk * 8  */
 
-	SYSTICK->LOAD = u32DelValueCpy<<3;
-
-	/*Enable Systick to start*/
-	SYSTICK->CTRL |=1;
-	while(! __SYSTICK_Flag_);
-	__SYSTICK_Flag_ =0;
-
-	/*Disable Systick to stop*/
-	SYSTICK->CTRL &= ~(1);
-}
-
-void Delay_ms(unsigned int u32DelValueCpy)
-{
-	while(u32DelValueCpy)
-	{
-		Delay_us(1000);
-		u32DelValueCpy--;
-	}
-}
-void Delay_s(unsigned int u32DelValueCpy)
-{
-	while(u32DelValueCpy)
-	{
-		Delay_ms(1000);
-		u32DelValueCpy--;
-	}
-}
-void SysTick_Handler(void)
-{
-	__SYSTICK_Flag_ =1;
-}
 #endif /* DELAY_H_ */
